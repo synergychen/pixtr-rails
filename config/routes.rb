28929dad91c10525e23fcdb2ca49100c2fe89582
Chen Huang <alan.tolearn@gmail.com>
2014-10-07 14:01:53 -0400

    User can create a new group
    add validates to group name
    render error messages if any error

--- config/routes.rb
+++ config/routes.rb
@@ -13,18 +13,5 @@ Rails.application.routes.draw do
     resources :comments, only: [:create]
   end
 
-  resources :groups, only: [:index]
-  # get "/" => "galleries#index"
-  #
-  # get "/galleries/new" => "galleries#new"
-  # post "/galleries" => "galleries#create"
-  #
-  # get "/galleries/:id" => "galleries#show"
-  #
-  # get "/galleries/:id/edit" => "galleries#edit"
-  # patch "/galleries/:id/update" => "galleries#update"
-  #
-  # get "/galleries/:id/delete" => "galleries#delete"
-  # patch "/galleries/:id/destroy" => "galleries#destroy"
-  # delete "/galleries/:id/destroy" => "galleries#destroy"
+  resources :groups, only: [:index, :new, :create]
 end
