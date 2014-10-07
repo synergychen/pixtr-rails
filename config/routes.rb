e3e6e64fdbbb6ab9f2db336fe0ebf1ec32da1452
Chen Huang <alan.tolearn@gmail.com>
2014-10-07 14:35:39 -0400

    User can show and edit group

--- config/routes.rb
+++ config/routes.rb
@@ -13,5 +13,5 @@ Rails.application.routes.draw do
     resources :comments, only: [:create]
   end
 
-  resources :groups, only: [:index, :new, :create]
+  resources :groups, only: [:index, :new, :create, :show, :edit, :update]
 end
