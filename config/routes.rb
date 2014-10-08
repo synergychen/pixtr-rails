25bed81500ca657a505f9a927236f00a48e4c90d
Chen Huang <alan.tolearn@gmail.com>
2014-10-08 11:46:50 -0400

    Change resources to resource
    We only need one path for create and destroy,
    and we could do that for group membership as well

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
 
   resources :images, only: [] do
     resources :comments, only: [:create]
-    resources :likes, only: [:create]
+    resource :like, only: [:create]
   end
 
   resources :groups, only: [:index, :new, :create, :show, :edit, :update] do
